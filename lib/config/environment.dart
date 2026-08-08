d46ca6bd99d3092d17ef46478f1b4c9c2ceef597
StorageArea483 <nimranaz378@gmail.com>
2026-08-08 16:17:30 +0500

    Ignore config and services directories

--- lib/config/environment.dart
+++
@@ -1,8 +0,0 @@
-class Environment {
-  static const String appwriteProjectId = '69f17cf0002d719084cd';
-
-  static const String appwriteProjectName = 'lifeline_data';
-
-  static const String appwritePublicEndpoint =
-      'https://fra.cloud.appwrite.io/v1';
-}
